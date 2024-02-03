e518cca9cf027415f9cebabc2a7d30471b345012
ledror <drorelmakias@gmail.com>
2024-02-03 22:19:12 +0200

    TODO: lsp xd

---
+++ lua/ledror/init.lua
@@ -0,0 +1,7 @@
+require("ledror.opt")
+require("ledror.mappings")
+require("ledror.init_lazy")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ lua/ledror/mappings.lua
@@ -0,0 +1,38 @@
+vim.g.mapleader = " "
+
+-- insert mode navigations
+vim.keymap.set("i", "<C-b>", "<Esc>^i")
+vim.keymap.set("i", "<C-e>", "<Esc>$a")
+
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+
+vim.keymap.set("n", "<Esc>", "<cmd> noh <CR>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- actually, these are the best remaps
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
