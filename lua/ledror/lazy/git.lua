e09cb1218af7d02dc13091d3164c8e616e7c2559
Dror Elmakias <drorelmakias@gmail.com>
2024-02-04 16:17:19 +0200

    lazygit and gitsigns

---
+++ lua/ledror/lazy/git.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
+        end
+    },
+
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = '+' },
+                    change = { text = '~' },
+                    delete = { text = '_' },
+                    topdelete = { text = '‾' },
+                    changedelete = { text = '~' },
+                },
+            })
+        end
+    }
+}
