cd33586efa60c2fe8f4fb44b05e0541f31ff8ee3
Dror Elmakias <drorelmakias@gmail.com>
2024-02-27 15:22:48 +0200

    in conform clang-format is clang_format (for obvious reasons). fixed tabwidth of it aswell

--- lua/ledror/lazy/conform.lua
+++ lua/ledror/lazy/conform.lua
@@ -3,13 +3,18 @@ return {
     config = function()
         local conform = require("conform")
         conform.setup({
+            formatters = {
+                clang_format = {
+                    args = "-style=\"{IndentWidth: 4,TabWidth: 4}\"",
+                },
+            },
             formatters_by_ft = {
                 lua = { "stylua" },
                 python = { "isort", "black" },
                 go = { "gofumpt", "goimports_reviser", "golines" },
-                c = { "clang-format" },
-                cpp = { "clang-format" },
-                objc = { "clang-format" },
+                c = { "clang_format" },
+                cpp = { "clang_format" },
+                objc = { "clang_format" },
             },
             format_on_save = {
                 timeout_ms = 500,
